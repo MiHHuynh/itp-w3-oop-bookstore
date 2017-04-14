e49dee8befe46acdc7872005372b7fe2dfc137e8
Michelle Huynh <michhh.12@gmail.com>
2017-04-14 07:22:02 +0000

    Adding methods and attributes to bookstore, author, and book

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,32 @@
 class Bookstore(object):
-    pass
-
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+    
+    def get_books(self):
+        return self.books
+    
+    def add_book(self, book):
+        self.books.append(book)
+    
+    def search_books(self, **kwargs):
+        pass
 
 class Author(object):
-    pass
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.books = []
+        
+    def get_books(self):
+        return self.books
+        
+    def add_book(self, book):
+        self.books.append(book)
 
 
 class Book(object):
-    pass
+    def __init__(self, title, **author):
+        self.title = title
+        self.author = author
+        self.author.add_book(self)
